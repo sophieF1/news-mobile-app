9690b9f04be1288186c5e80d6f8cf331400e8342
sophieF1 <sophie.field98@gmail.com>
2019-12-27 18:26:49 +0000

    initial commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,31 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+
+import TopStories from '../views/TopStories.vue';
+import Astrology from '../views/Astrology.vue';
+import Favourites from '../views/Favourites.vue';
+
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
-
-const router = new VueRouter({
+export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: '/',
+      name: 'top-stories',
+      component: TopStories
+    },
+    {
+      path: '/astrology',
+      name: 'astrology',
+      component: Astrology
+    },
+    {
+      path: '/my-favourites',
+      name: 'favourites',
+      component: Favourites
+    }
+  ]
 });
-
-export default router;
